db4d26ecce3cdae7082725cd3fdd55a9b0e767b1
Bora <173380240+tyy-dev@users.noreply.github.com>
2025-12-17 12:57:46 +0100

    Delete Core/Builders/MenuBarBuilder.cs

--- Core/Builders/MenuBarBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Terminal.Gui.Input;
-using Terminal.Gui.ViewBase;
-using Terminal.Gui.Views;
-
-namespace TerminalGui.Extensions.Core.Builders;
-
-//public class MenuBarBuilder(MenuBar menuBar) : ViewBuilder<MenuBar>(menuBar)
-//{
-//}
